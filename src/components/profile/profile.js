2232e317b294e73a9572accb9b9add727782c869
Umayanga <umayangag@gmail.com>
2019-09-21 18:05:07 +0530

    entity not found profile error handle

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -28,61 +28,77 @@ class Profile extends Component {
 
   render() {
     const {classes, loadedEntity, internalLinks, getInternalLinks, relatedResults} = this.props;
-    return (
-      <Grid className={classes.container} container width={1}>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>
-            Internal Links
-            <Box height="90vh" overflow="auto">
-              <TrendingList results={internalLinks} getResults={getInternalLinks}
-                            searchParam={loadedEntity ? loadedEntity.title : ""}/>
-            </Box>
-          </Paper>
+
+    if (loadedEntity == null) {
+      return (
+        <Grid className={classes.container} container width={1}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Typography component="p">
+                Document not found
+              </Typography>
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid item xs={9}>
-          <Paper className={classes.paper}>
-            <Grid container width={1}>
-              <Grid item>
-                <Avatar alt={loadedEntity.title}
-                        src={loadedEntity.image_url === "" ? "avatar.png" : loadedEntity.image_url}
-                        className={classes.bigAvatar}/>
-              </Grid>
-              <Grid item xs={10}>
-                <Typography variant="h4" gutterBottom>
-                  {loadedEntity.title}
-                </Typography>
-                <Box maxHeight="70vh" overflow="auto">
-                  <table>
-                    <tbody>
-                    {loadedEntity.attributes ? loadedEntity.attributes.map((attribute) => (
-                      <FormattedContent key={attribute.name} content={attribute}/>
-                    )) : null}
-                    </tbody>
-                  </table>
-                </Box>
-                <Typography variant="subtitle1">
-                  {loadedEntity.categories ? loadedEntity.categories.map((tag) =>
-                    <Link key={tag} className={classes.link} to={"/search/" + tag+":"}>
-                      {tag}
-                    </Link>
-                  ) : null}
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item xs={12}>
-              <Divider variant="inset" component="div"/>
-            </Grid>
-            <Grid item xs={11}>
-              <br/>
-              Related Articles
-              <Box maxHeight="70vh" overflow="auto">
-                <MainContentList listItems={relatedResults}/>
+      )
+    } else {
+      return (
+        <Grid className={classes.container} container width={1}>
+
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>
+              Internal Links
+              <Box height="90vh" overflow="auto">
+                <TrendingList results={internalLinks} getResults={getInternalLinks}
+                              searchParam={loadedEntity.title}/>
               </Box>
-            </Grid>
-          </Paper>
+            </Paper>
+          </Grid>
+          < Grid item xs={9}>
+            <Paper className={classes.paper}>
+              <Grid container width={1}>
+                <Grid item>
+                  <Avatar alt={loadedEntity.title}
+                          src={loadedEntity.image_url === "" ? "avatar.png" : loadedEntity.image_url}
+                          className={classes.bigAvatar}/>
+                </Grid>
+                <Grid item xs={10}>
+                  <Typography variant="h4" gutterBottom>
+                    {loadedEntity.title}
+                  </Typography>
+                  <Box maxHeight="70vh" overflow="auto">
+                    <table>
+                      <tbody>
+                      {loadedEntity.attributes ? loadedEntity.attributes.map((attribute) => (
+                        <FormattedContent key={attribute.name} content={attribute}/>
+                      )) : null}
+                      </tbody>
+                    </table>
+                  </Box>
+                  <Typography variant="subtitle1">
+                    {loadedEntity.categories ? loadedEntity.categories.map((tag) =>
+                      <Link key={tag} className={classes.link} to={"/search/" + tag + ":"}>
+                        {tag}
+                      </Link>
+                    ) : null}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item xs={12}>
+                <Divider variant="inset" component="div"/>
+              </Grid>
+              <Grid item xs={11}>
+                <br/>
+                Related Articles
+                <Box maxHeight="70vh" overflow="auto">
+                  <MainContentList listItems={relatedResults}/>
+                </Box>
+              </Grid>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    );
+      );
+    }
   }
 }
 
