466f1e43d4b273f4925239e66f609d55bd7b4b1c
umayanga <umayangag@gmail.com>
2022-03-17 12:07:20 +0530

    setupProxy bug fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,6 @@
-import {ApiRoutes, translatorRoutes} from "./server";
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use(createProxyMiddleware(ApiRoutes.images, { target: process.env.REACT_APP_SERVER_URL , changeOrigin:true }));
-  app.use(createProxyMiddleware(translatorRoutes.translate, { target: process.env.TRANSLATOR_URL }));
+module.exports = function (app) {
+  app.use(createProxyMiddleware("/images", {target: process.env.REACT_APP_SERVER_URL, changeOrigin: true}));
+  app.use(createProxyMiddleware('/translate', {target: process.env.TRANSLATOR_URL}));
 };
